7d466a83127d93138d0d5aef8c259991e405e78b
João Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2024-11-17 09:32:07 -0300

    Add Files. to project.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace ConsoleAppUsing{
+   internal abstract class Program{
+      private static void Main()
+      {
+          const string path = @"C:\temp\FIle.txt";
+          using (var fs = new FileStream(path, FileMode.Open)){
+              using (var sr = new StreamReader(fs)){
+                  while (!sr.EndOfStream)
+                  {
+                      Console.WriteLine(sr.ReadLine());
+                  }
+              }
+          }
+      }
+    }
+}
